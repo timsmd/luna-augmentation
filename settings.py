5c79adb34aa0bbef00ce25ce4e8b4e3ccb8637f1
anastasiia klimova <klimovanastya95@gmail.com>
2019-05-06 16:34:48 +0300

    Settings fixed

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
-    size = 64
-    no_slices = 1
+    size = 512
+    no_slices = 20
     data_dir = 'DATAFOLDER/stage1/'
     labels_file = 'DATAFOLDER/stage1_labels/stage1_labels.csv'
     aug_counter = 3
